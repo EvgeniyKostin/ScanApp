f3aa8fcf1c3e6c823f95edb787cbc5543d035bf6
Evgeniy Kostin <xqvynfs6f8@privaterelay.appleid.com>
2024-05-11 09:22:41 +0300

    Initial Commit

---
+++ ScanApp/ScanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanAppApp.swift
+//  ScanApp
+//
+//  Created by Evgeniy Kostin on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
